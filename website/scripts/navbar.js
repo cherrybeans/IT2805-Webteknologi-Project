d5f9486d0659553f1abbf67972ec301738dd6c54
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2017-11-10 11:23:10 +0100

    Closes #3, closes #17, closes #19, closes #21. Fix footer and heaps of small fixes all around

--- website/scripts/navbar.js
+++ website/scripts/navbar.js
@@ -1,6 +1,6 @@
 /*
 FILE NAME: scripts/Navbar.html
-WRITTEN BY: Kristoffer Håkonsen
+WRITTEN BY: Kristoffer Hï¿½konsen
 A big thanks to w3schools.com for inspiraton on how to create the dropdown navbar
 
 WHEN: October/November 2017
@@ -11,65 +11,57 @@ PURPOSE: This page contains the Javascript which makes the navbar expand on clic
 btns = ["", "aboutbtn", "mediabtn", "contactbtn"];
 
 //Toggles showing and not showing the dropdowns
-/* We would like to thank the w3school for their idea about appending "show" to make the dropdown appear and disaper. 
+/* We would like to thank the w3school for their idea about appending "show" to make the dropdown appear and disaper.
 The idea came from their articles: https://www.w3schools.com/howto/howto_js_dropdown.asp */
 function dropdownFunction(buttonclicked) {
-	
-    //Checks if the clicked button is already showing a dropdown menu
-	if(document.getElementById(btns[buttonclicked]).classList.contains("show")){
-		removeDropdown();
-		
-	}
-    else{
-		//Removes all dropdowns
-		removeDropdown();
+  //Checks if the clicked button is already showing a dropdown menu
+  if (document.getElementById(btns[buttonclicked]).classList.contains("show")) {
+    removeDropdown();
+  } else {
+    //Removes all dropdowns
+    removeDropdown();
 
-		//Shows the dropdown
-		document.getElementById(btns[buttonclicked]).classList.toggle("show");
-
-		}
-	}
-
+    //Shows the dropdown
+    document.getElementById(btns[buttonclicked]).classList.toggle("show");
+  }
+}
 
 // If the screen is clicked outside the dropdown buttons or outside the small dropdown icon
 
-window.onclick = function(event){
-    if (!event.target.matches('.dropbtn')) {
-        removeDropdown();
-    }
-   if  (!event.target.matches('.icon')){
-       removeSmallDropdown();
-    }
-}
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    removeDropdown();
+  }
+  if (!event.target.matches(".icon")) {
+    removeSmallDropdown();
+  }
+};
 
 //Removed all the regular dropdowns
 /*Thanks to the w3school for help on this function. We used the principles from https://www.w3schools.com/howto/howto_js_dropdown.asp to create the removeDropdown() function. */
 function removeDropdown() {
-   
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    
-    for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+
+  for (var i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains("show")) {
+      openDropdown.classList.remove("show");
     }
+  }
 }
 
 /*Removes the small dropdown*/
 function removeSmallDropdown() {
-    var dropdowns = document.getElementsByClassName("smalldropdown");
-    for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
+  var dropdowns = document.getElementsByClassName("smalldropdown");
+  for (var i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains("show")) {
+      openDropdown.classList.remove("show");
     }
+  }
 }
-
-
 
 //Toggles the small dropdown menu
 function smallDropDown() {
-    document.getElementById("smalldropdownbtn").classList.toggle('show');
+  document.getElementById("smalldropdownbtn").classList.toggle("show");
 }
