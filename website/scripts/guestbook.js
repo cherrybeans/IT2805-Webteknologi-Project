27f7549bd7ce9ccfced818cea4948a81345ae75f
Håvard Bjerke <h0wy36@gmail.com>
2017-11-13 13:43:34 +0100

    - Add alt-attributes to some images  - Fix validation error with <emph> in guestbook

--- website/scripts/guestbook.js
+++ website/scripts/guestbook.js
@@ -26,7 +26,7 @@ function gb_loadComments() {
 
     var element = document.createElement("div");
 
-    var dateL = document.createElement("emph");
+    var dateL = document.createElement("i");
     var nameL = document.createElement("strong");
     var textL = document.createElement("div");
 
