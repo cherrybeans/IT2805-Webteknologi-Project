c8940a8cf803be8a975a80d2acb225852d20654b
Kristoffer Håkonsen <kristoffer@hakonsen.com>
2017-11-13 11:07:43 +0100

    Added top level comment

--- website/scripts/splash_image.js
+++ website/scripts/splash_image.js
@@ -1,3 +1,11 @@
+/*
+  FILE NAME: scripts/splash_image.js
+  WRITTEN BY: Caroline Stensland Selte
+  WHEN: October/November 2017
+  PURPOSE: Splashes the images in, making it look cooler
+*/
+
+
 function si_load() {
   var image = document.getElementById("bulletin");
   image.classList.add("splash");
